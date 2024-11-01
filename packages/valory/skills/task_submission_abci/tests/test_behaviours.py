b12453c15ca743c8cdd02c009b8e51969c2ad2c2
KahanMajmudar <kahan.m.btechi16@ahduni.edu.in>
2024-11-01 06:23:05 +0000

    added tests for TaskPoolingBehaviour

--- packages/valory/skills/task_submission_abci/tests/test_behaviours.py
+++ packages/valory/skills/task_submission_abci/tests/test_behaviours.py
@@ -22,7 +22,7 @@ import logging
 import platform
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Dict, List, Callable, Optional, Type
+from typing import Any, Dict, List, Generator, Callable, Optional, Type
 from unittest import mock
 
 import pytest
@@ -41,6 +41,7 @@ from packages.valory.skills.task_submission_abci.behaviours import (
 from packages.valory.skills.task_submission_abci.rounds import (
     Event,
     SynchronizedData,
+    FinishedWithoutTasksRound,
 )
 from packages.valory.contracts.gnosis_safe.contract import GnosisSafeContract
 from packages.valory.protocols.contract_api import ContractApiMessage
@@ -77,8 +78,8 @@ class BehaviourTestCase:
     initial_data: Dict[str, Any]
     ok_reqs: List[Callable]
     err_reqs: List[Callable]
-    expected_log: str
-    expected_log_level: int
+    expected_logs: List[str]
+    expected_log_levels: List[int]
     event: Event = Event.DONE
     next_behaviour_class: Optional[Type[BaseBehaviour]] = None
 
@@ -144,19 +145,131 @@ class TestTaskPoolingBehaviour(BaseTaskSubmissionTest):
         participants=_SAFE_OWNERS,
         consensus_threshold=3,
     )
+    _DUMMY_DONE_TASKS = [
+        {
+            "request_id": 58758105316598692706519173691512343367950130276409284865927422102201674760225,
+            "mech_address": "0x262FF09Bf2496fa62264bA71dFBE9b85794193A7",
+            "task_executor_address": "0x264968d975cAA715e1D65c2b5902fE9ea64E6F28",
+            "tool": "prediction-online",
+            "request_id_nonce": "None",
+            "is_marketplace_mech": False,
+            "task_result": "43d21586462cbd9ce086ca7d0c5559316378d08c60599f71b48b7cf2dd3fd063",
+        },
+        {
+            "request_id": 85336656703727246191869830228415812710148856408201745592663531215206922045768,
+            "mech_address": "0x262FF09Bf2496fa62264bA71dFBE9b85794193A7",
+            "task_executor_address": "0x264968d975cAA715e1D65c2b5902fE9ea64E6F28",
+            "tool": "prediction-online",
+            "request_id_nonce": None,
+            "is_marketplace_mech": False,
+            "task_result": "b711c818be38a1a4803bc43c29e44b70ea02b0888ed553b5fed7111cd27e6684",
+        },
+        {
+            "request_id": 100045748490114249017905977519000015185197459576092770991271139356043347620871,
+            "mech_address": "0x262FF09Bf2496fa62264bA71dFBE9b85794193A7",
+            "task_executor_address": "0x264968d975cAA715e1D65c2b5902fE9ea64E6F28",
+            "tool": None,
+            "request_id_nonce": None,
+            "is_marketplace_mech": False,
+            "task_result": "c84c814458b4af5d98fea6d9b19aede3ea370b49b62ad94cdc8b9fa60e9058e3",
+        },
+    ]
 
-    _GET_LAST_TX_HASH_ERR = "Last tx status is: "
+    _GET_LAST_TX_HASH_INFO = "Last tx status is: {status}"
+    _GET_TASK_REMOVE_INFO = "Tasks [] has already been submitted. Removing them from the list of tasks to be processed."
+
+    def dummy_get_done_tasks_wrapper(
+        self,
+        tasks: List[dict],
+    ) -> mock._patch:
+        """Mock BaseBehaviour.get_done_tasks method."""
+
+        def dummy_get_done_tasks(
+            *_: Any, **__: Any
+        ) -> Generator[None, None, Optional[List[dict]]]:
+            """Dummy `get_done_tasks` method."""
+            yield
+            return tasks
+
+        return mock.patch.object(
+            self.behaviour.current_behaviour,
+            "get_done_tasks",
+            side_effect=dummy_get_done_tasks,
+        )
+
+    def dummy_remove_tasks_wrapper(
+        self,
+    ) -> mock._patch:
+        """Mock BaseBehaviour.remove_tasks method."""
+
+        def dummy_remove_tasks(
+            *_: Any, **__: Any
+        ) -> Generator[None, None, Optional[List[dict]]]:
+            """Dummy `remove_tasks` method."""
+            yield
+            return []
+
+        return mock.patch.object(
+            self.behaviour.current_behaviour,
+            "remove_tasks",
+            side_effect=dummy_remove_tasks,
+        )
 
     @pytest.mark.parametrize(
         "test_case",
         [
             BehaviourTestCase(
                 name="Last final tx hash is not present",
-                initial_data=_INITIAL_DATA,
+                initial_data=_INITIAL_DATA | {"done_tasks": []},
                 ok_reqs=[],
                 err_reqs=[],
-                expected_log=_GET_LAST_TX_HASH_ERR,
-                expected_log_level=logging.INFO,
+                expected_logs=[_GET_LAST_TX_HASH_INFO.format(status=False)],
+                expected_log_levels=[logging.INFO],
+                event=Event.NO_TASKS,
+                next_behaviour_class=make_degenerate_behaviour(
+                    FinishedWithoutTasksRound
+                ),
+            ),
+            BehaviourTestCase(
+                name="Last final tx hash is None",
+                initial_data=_INITIAL_DATA | {"done_tasks": []},
+                ok_reqs=[],
+                err_reqs=[],
+                expected_logs=[_GET_LAST_TX_HASH_INFO.format(status=False)],
+                expected_log_levels=[logging.INFO],
+                event=Event.NO_TASKS,
+                next_behaviour_class=make_degenerate_behaviour(
+                    FinishedWithoutTasksRound
+                ),
+            ),
+            BehaviourTestCase(
+                name="Last final tx hash is present",
+                initial_data=_INITIAL_DATA
+                | {"done_tasks": [], "final_tx_hash": _MOCK_TX_HASH},
+                ok_reqs=[],
+                err_reqs=[],
+                expected_logs=[
+                    _GET_LAST_TX_HASH_INFO.format(status=True),
+                    _GET_TASK_REMOVE_INFO,
+                ],
+                expected_log_levels=[logging.INFO, logging.INFO],
+                event=Event.NO_TASKS,
+                next_behaviour_class=make_degenerate_behaviour(
+                    FinishedWithoutTasksRound
+                ),
+            ),
+            BehaviourTestCase(
+                name="Behaviour ends with done tasks",
+                initial_data=_INITIAL_DATA
+                | {"done_tasks": _DUMMY_DONE_TASKS, "final_tx_hash": _MOCK_TX_HASH},
+                ok_reqs=[],
+                err_reqs=[],
+                expected_logs=[
+                    _GET_LAST_TX_HASH_INFO.format(status=True),
+                ],
+                expected_log_levels=[logging.INFO],
+                event=Event.DONE,
+                next_behaviour_class=TransactionPreparationBehaviour,
             ),
         ],
     )
@@ -169,7 +282,9 @@ class TestTaskPoolingBehaviour(BaseTaskSubmissionTest):
 
         with mock.patch.object(
             self.behaviour.current_behaviour.context.logger, "log"
-        ) as mock_logger:
+        ) as mock_logger, self.dummy_get_done_tasks_wrapper(
+            tasks=test_case.initial_data["done_tasks"]
+        ), self.dummy_remove_tasks_wrapper():
             self.behaviour.act_wrapper()
 
             # apply the OK mocks first
@@ -180,28 +295,32 @@ class TestTaskPoolingBehaviour(BaseTaskSubmissionTest):
             for err_req in test_case.err_reqs:
                 err_req(self, error=True)
 
-            print(f"mock logs: {mock_logger.call_args_list}")
+            assert len(test_case.expected_logs) == len(test_case.expected_log_levels)
 
-            log_found = False
-            for log_args in mock_logger.call_args_list:
-                if platform.python_version().startswith("3.7"):
-                    actual_log_level, actual_log = log_args[0][:2]
-                else:
-                    actual_log_level, actual_log = log_args.args[:2]
+            for log, log_level in zip(
+                test_case.expected_logs, test_case.expected_log_levels
+            ):
 
-                if actual_log.startswith(test_case.expected_log):
-                    assert actual_log_level == test_case.expected_log_level, (
-                        f"{test_case.expected_log} was expected to log on {test_case.expected_log_level} log level, "
-                        f"but logged on {log_args[0]} instead."
+                log_found = False
+                for log_args in mock_logger.call_args_list:
+                    if platform.python_version().startswith("3.7"):
+                        actual_log_level, actual_log = log_args[0][:2]
+                    else:
+                        actual_log_level, actual_log = log_args.args[:2]
+
+                    if str(actual_log).startswith(log):
+                        assert actual_log_level == log_level, (
+                            f"{log} was expected to log on {log_level} log level, "
+                            f"but logged on {log_args[0]} instead."
+                        )
+                        log_found = True
+                        break
+
+                if not log_found:
+                    raise AssertionError(
+                        f'Expected log message "{log}" was not found in captured logs: '
+                        f"{mock_logger.call_args_list}."
                     )
-                    log_found = True
-                    break
-
-            if not log_found:
-                raise AssertionError(
-                    f'Expected log message "{test_case.expected_log}" was not found in captured logs: '
-                    f"{mock_logger.call_args_list}."
-                )
 
         if len(test_case.err_reqs) == 0:
             # no mocked requests fail,
